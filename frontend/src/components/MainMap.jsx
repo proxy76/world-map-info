863ca2a945d1c01647ba76a2a3a3a053accb9d3b
Radulescu Razvan <proxy.razvan@gmail.com>
2025-02-12 19:32:48 +0200

    edit: map style

--- frontend/src/components/MainMap.jsx
+++ frontend/src/components/MainMap.jsx
@@ -1,35 +1,26 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { ComposableMap, Geographies, Geography } from "react-simple-maps";
-
-import "../styles/main.scss";
 
 const MainMap = () => (
   <div>
     <ComposableMap>
-      <Geographies geography="https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json">
-        {({ geographies }) =>
-          geographies.map((geo) => (
+      <Geographies geography="/features.json">
+        {({ geographies }) => {
+
+          return geographies.map((geo) => (
             <Geography 
               key={geo.rsmKey} 
               geography={geo}   
               style={{
-                default: { fill: "#D6D6DA", stroke: "#000", outline: "none" },
-                hover: { fill: "#F53", cursor: "pointer" },
-                pressed: { fill: "#E42", outline: "none", stroke: "none"},
+                default: { fill: "#D6D6DA", stroke: "none", outline: "none" },
               }}
-              onFocus={(e) => {
-                e.preventDefault();
-                e.target.style.outline = 'none';
-                e.target.style.stroke = 'none';
-              }}
+              onFocus={(e) => e.target.blur()} // Prevents focus outline
             />
-          ))
-        }
+          ));
+        }}
       </Geographies>
     </ComposableMap>
   </div>
 );
 
 export default MainMap;
-
