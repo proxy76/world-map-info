2893f4417078fca6bbf5f3732999d61c3baa1cdf
Radulescu Razvan <proxy.razvan@gmail.com>
2025-02-12 20:16:52 +0200

    tuta

--- frontend/src/components/MainMap.jsx
+++ frontend/src/components/MainMap.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+import Menu from "./Menu";
 
 const MainMap = () => {
   const [selectedCountry, setSelectedCountry] = useState(null);
+  const [ isSelected, setIsSelected ] = useState(false);
 
   return (
     <div>
@@ -10,31 +12,34 @@ const MainMap = () => {
         <Geographies geography="/features.json">
           {({ geographies }) =>
             geographies.map((geo) => {
-              const isSelected = selectedCountry === geo.id;
+              setIsSelected(selectedCountry === geo.id);
               return (
-                <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  onClick={() => setSelectedCountry(geo.id)}
-                  style={{
-                    default: {
-                      fill: isSelected ? "#FF5733" : "#D6D6DA",
-                      stroke: "#000",
-                      outline: "none",
-                    },
-                    hover: {
-                      fill: "#A8A8A8",
-                      stroke: "#000",
-                      outline: "none",
-                    },
-                    pressed: {
-                      fill: "#FF5733",
-                      stroke: "#000",
-                      outline: "none",
-                    },
-                  }}
-                  onFocus={(e) => e.target.blur()} // Prevents focus outline
-                />
+                <>
+                  { isSelected && <Menu country={geo} /> }
+                  <Geography
+                    key={geo.rsmKey}
+                    geography={geo}
+                    onClick={() => setSelectedCountry(geo.id)}
+                    style={{
+                      default: {
+                        fill: isSelected ? "#FF5733" : "#D6D6DA",
+                        stroke: "#000",
+                        outline: "none",
+                      },
+                      hover: {
+                        fill: "#A8A8A8",
+                        stroke: "#000",
+                        outline: "none",
+                      },
+                      pressed: {
+                        fill: "#FF5733",
+                        stroke: "#000",
+                        outline: "none",
+                      },
+                    }}
+                    onFocus={(e) => e.target.blur()} // Prevents focus outline
+                  />
+                </>
               );
             })
           }
---
+++ frontend/src/components/Menu.jsx
@@ -0,0 +1,7 @@
+export default function Menu({ country }) {
+    return (
+        <div className="tuta">
+            <h1>{country.properties.name}</h1>
+        </div>
+    );
+}
