b76e7d3aa011505af64db74728e7cd84e4bf2d75
Radulescu Razvan <proxy.razvan@gmail.com>
2025-02-13 10:31:17 +0200

    add: header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,33 @@
+import React, { useState } from 'react';
+import './Header.css';
+
 export default function Header() {
+    const [showPopup, setShowPopup] = useState(false);
+
+    const togglePopup = () => {
+        setShowPopup(!showPopup);
+    };
+
     return (
-        <div className="header">
-            <h1>World Map</h1>
+        <div className="wrapper">
+            <div className="header">
+                <h1 className="title">World Map</h1>
+                <div className="profile-container">
+                    <img
+                        src="path/to/profile-picture.jpg"
+                        alt="Profile"
+                        className="profile-picture"
+                        onClick={togglePopup}
+                    />
+                    {showPopup && (
+                        <div className="popup">
+                            <p>Login</p>
+                            <p>Logout</p>
+                        </div>
+                    )}
+                </div>
+            </div>
         </div>
-    )
+
+    );
 }
